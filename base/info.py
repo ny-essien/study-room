1fbb3524854fc03ef69b225c9213e80788aa6741
Nsikan4001 <67320242+Nsikan4001@users.noreply.github.com>
2023-02-16 11:39:34 +0100

    Update info.py

--- base/info.py
+++ base/info.py
@@ -2,5 +2,5 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'fabbyrehoboth123@gmail.com'
-EMAIL_HOST_PASSWORD = 'essien4you'
+EMAIL_HOST_PASSWORD = '***********'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
